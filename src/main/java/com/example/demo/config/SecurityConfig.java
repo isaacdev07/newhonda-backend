88dab7b94a1f73da039a40d74cba4ccf08c98cc9
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-02 18:58:03 -0300

    Feat: security for endpoints and bean of password encoder

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	//bean para deixar a rota de usuarios publica
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/create").permitAll() 
+                        .anyRequest().authenticated() 
+                )
+                .build();
+    }
+
+    // hash de senha password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}

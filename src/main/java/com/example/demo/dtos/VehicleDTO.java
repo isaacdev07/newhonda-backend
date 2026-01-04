0c4194ff46b874a56f613d2cfb40f47b2eac29ee
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-03 22:33:43 -0300

    Feat: dto for entity vehicle

---
+++ src/main/java/com/example/demo/dtos/VehicleDTO.java
@@ -0,0 +1,101 @@
+package com.example.demo.dtos;
+
+import com.example.demo.enums.StateVehicle;
+import com.example.demo.enums.TypeVehicle;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class VehicleDTO {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nameVehicle;
+	private int year;
+	private String kmsDriven;
+	private TypeVehicle typeVehicle;
+	private StateVehicle stateVehicle;
+	
+	
+	public VehicleDTO(Long id, String nameVehicle, int year, String kmsDriven, TypeVehicle typeVehicle,
+			StateVehicle stateVehicle) {
+		this.id = id;
+		this.nameVehicle = nameVehicle;
+		this.year = year;
+		this.kmsDriven = kmsDriven;
+		this.typeVehicle = typeVehicle;
+		this.stateVehicle = stateVehicle;
+	}
+
+
+	public VehicleDTO() {
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNameVehicle() {
+		return nameVehicle;
+	}
+
+
+	public void setNameVehicle(String nameVehicle) {
+		this.nameVehicle = nameVehicle;
+	}
+
+
+	public int getYear() {
+		return year;
+	}
+
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+	public String getKmsDriven() {
+		return kmsDriven;
+	}
+
+
+	public void setKmsDriven(String kmsDriven) {
+		this.kmsDriven = kmsDriven;
+	}
+
+
+	public TypeVehicle getTypeVehicle() {
+		return typeVehicle;
+	}
+
+
+	public void setTypeVehicle(TypeVehicle typeVehicle) {
+		this.typeVehicle = typeVehicle;
+	}
+
+
+	public StateVehicle getStateVehicle() {
+		return stateVehicle;
+	}
+
+
+	public void setStateVehicle(StateVehicle stateVehicle) {
+		this.stateVehicle = stateVehicle;
+	}
+	
+	
+	
+
+}

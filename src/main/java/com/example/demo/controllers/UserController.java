5e41e5d0e69e86cb64478f714718176218552b15
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-05 22:30:10 -0300

    Feat: new enpoint for login

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -3,13 +3,18 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dtos.AuthenticationDTO;
+import com.example.demo.dtos.LoginResponseDTO;
 import com.example.demo.dtos.UserDTO;
 import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+import com.example.demo.services.TokenService;
 import com.example.demo.services.UserService;
 
 import jakarta.validation.Valid;
@@ -21,6 +26,15 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TokenService tokenService;
+	
 	//endpoint para criar um novo usuario
 	@PostMapping("/create")
 	public ResponseEntity<User> createNewUser(@Valid @RequestBody UserDTO userDTO) {
@@ -29,4 +43,22 @@ public class UserController {
 	    return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
 	}
 
+	@PostMapping("/login")
+	public ResponseEntity login(@RequestBody AuthenticationDTO data) {
+		
+		//procura o usuario pelo email
+		User user = this.repository.findByEmail(data.email());
+		
+		//verifica se a senha e/ou email estao corretos
+		if(user != null && passwordEncoder.matches(data.password(), user.getPassword())) {
+			String token = tokenService.generateToken(user);
+			
+			//tudo ok retorna o token
+			return ResponseEntity.ok(new LoginResponseDTO(token));
+		}
+		//nao passou do if mensagem de erro
+		return ResponseEntity.badRequest().build();		
+		
+	}
+	
 }

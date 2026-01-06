f42ef907d75796d655a1b80b70c73d3b6593b306
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-05 22:37:15 -0300

    Feat: service to generate token jwt

---
+++ src/main/java/com/example/demo/services/TokenService.java
@@ -0,0 +1,53 @@
+package com.example.demo.services;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.demo.entities.User;
+
+@Service
+public class TokenService {
+	
+	@Value("${api.security.token.secret}")
+	private String secret;
+
+	
+	public String generateToken(User user) {
+		
+		try {
+			
+		//define o algoritimo com a senha
+		Algorithm algorithm = Algorithm.HMAC256(secret);
+		
+		String token = JWT.create()
+				.withIssuer("newHonda-api")
+				.withSubject(user.getEmail()) //identificação do usuario
+				.withExpiresAt(genExpirationDate()) //tempo de expiração do token
+				.sign(algorithm); //assina e finaliza
+		
+		return token;
+		
+		//excessao para tratar possivel erro
+		} catch (JWTCreationException exception) {
+		
+		throw new RuntimeException("Erro ao gerar token", exception);
+	}
+	
+	}
+	
+	//pega a data e deixa a validade do token em 2 horas
+	private Instant genExpirationDate() {
+		
+		return LocalDateTime.now()
+				.plusHours(2)
+				.toInstant(ZoneOffset.of("-03:00"));
+	}
+	
+}

07f31bd653eb424a2161ea2ca244abc09a04a7a6
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-06 23:26:42 -0300

    Feat: update routes now get vehicles need autentication hello-world, create user and login publics

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	SecurityFilter securityFilter;
 	
 	//bean para deixar as rotas publicas
     @Bean
@@ -18,9 +24,16 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/create", "/user/login", "/vehicle/create", "/hello-world").permitAll() 
+                		//get hello world para verificar se a api esta rodando
+                        .requestMatchers("/hello-world").permitAll() 
+                        //post de cadastro e logins publicos
+                        .requestMatchers(HttpMethod.POST, "/user/create", "/user/login").permitAll()
+                        //get nos veiculos somente autenticado
+                        .requestMatchers(HttpMethod.GET, "/vehicle").authenticated()
+                        
                         .anyRequest().authenticated() 
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 

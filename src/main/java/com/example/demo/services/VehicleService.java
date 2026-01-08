8ad613c201e213242e1ad51c053cbed32f1b787e
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-08 00:46:51 -0300

    Feat: update method getAllVehicles now we have a try catch to get onle we want

--- src/main/java/com/example/demo/services/VehicleService.java
+++ src/main/java/com/example/demo/services/VehicleService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.dtos.VehicleDTO;
 import com.example.demo.entities.Vehicle;
+import com.example.demo.enums.TypeVehicle;
 import com.example.demo.repositories.VehicleRepository;
 
 @Service
@@ -31,8 +33,22 @@ public class VehicleService {
 		
 	}
 	
-	public List<Vehicle> getAllVehicles(){
+	public List<Vehicle> getAllVehicles(String typeVehicle){
 		
+		if(typeVehicle != null && !typeVehicle.isEmpty()) {
+			
+			try {
+				//trasforma o string no enum para o banco poder ler
+				TypeVehicle tipoConvertido = TypeVehicle.valueOf(typeVehicle.toUpperCase());
+				//chama o enum correto para o banco
+				return vehicleRepository.findByTypeVehicle(tipoConvertido);
+				//se chega um valor nao existente retorna uma lista vazia
+			} catch (IllegalArgumentException e) {
+				return Collections.emptyList();
+			}
+			
+		}
+		//caso contrario retorna tudo
 		return vehicleRepository.findAll();
 	}
 	

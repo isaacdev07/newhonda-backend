0e19425b86f908e181ccdf8aec8d598f44a9cb3f
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-08 00:45:25 -0300

    Feat: add method to get using typeVehicle

--- src/main/java/com/example/demo/repositories/VehicleRepository.java
+++ src/main/java/com/example/demo/repositories/VehicleRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Vehicle;
+import com.example.demo.enums.TypeVehicle;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+	
+	List<Vehicle> findByTypeVehicle(TypeVehicle typeVehicle);
 
 }

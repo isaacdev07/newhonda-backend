12a6a93c124735edf89b02419f8690f9f8c06321
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-09 00:04:10 -0300

    Feat: add methods to filter now we can filter about stateVehicle and merge all these filters

--- src/main/java/com/example/demo/repositories/VehicleRepository.java
+++ src/main/java/com/example/demo/repositories/VehicleRepository.java
@@ -5,10 +5,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Vehicle;
+import com.example.demo.enums.StateVehicle;
 import com.example.demo.enums.TypeVehicle;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 	
+	//listar veiculos de determidados tipos (carro ou moto)
 	List<Vehicle> findByTypeVehicle(TypeVehicle typeVehicle);
+	
+	//listar veiculos de determinados estados (novo ou usado)
+	List<Vehicle> findByStateVehicle(StateVehicle stateVehicle);
 
+	//combinar os filtros para buscar dois tipos ao mesmo tempo
+	List<Vehicle> findByTypeVehicleAndStateVehicle(TypeVehicle typeVehicle,StateVehicle stateVehicle);
 }

e9184643fa626c68d6eac596878ff9e201c331e5
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-11 22:47:44 -0300

    Feat: change type list to page

--- src/main/java/com/example/demo/repositories/VehicleRepository.java
+++ src/main/java/com/example/demo/repositories/VehicleRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repositories;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Vehicle;
@@ -11,11 +11,11 @@ import com.example.demo.enums.TypeVehicle;
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 	
 	//listar veiculos de determidados tipos (carro ou moto)
-	List<Vehicle> findByTypeVehicle(TypeVehicle typeVehicle);
+	Page<Vehicle> findByTypeVehicle(TypeVehicle typeVehicle, Pageable pageable);
 	
 	//listar veiculos de determinados estados (novo ou usado)
-	List<Vehicle> findByStateVehicle(StateVehicle stateVehicle);
+	Page<Vehicle> findByStateVehicle(StateVehicle stateVehicle, Pageable pageable);
 
 	//combinar os filtros para buscar dois tipos ao mesmo tempo
-	List<Vehicle> findByTypeVehicleAndStateVehicle(TypeVehicle typeVehicle,StateVehicle stateVehicle);
+	Page<Vehicle> findByTypeVehicleAndStateVehicle(TypeVehicle typeVehicle,StateVehicle stateVehicle, Pageable pageable);
 }

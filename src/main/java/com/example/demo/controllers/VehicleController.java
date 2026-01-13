753914244474f2a411469865ef7fdd9cd1563e43
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-12 22:31:29 -0300

    Feat: add enpoints for update and delete vehicles

--- src/main/java/com/example/demo/controllers/VehicleController.java
+++ src/main/java/com/example/demo/controllers/VehicleController.java
@@ -6,8 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,22 +25,39 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("vehicle")
 public class VehicleController {
-	
+
 	@Autowired
 	private VehicleService vehicleService;
-	
+
 	@PostMapping("/create")
-	public ResponseEntity<Vehicle> createNewUser(@Valid @RequestBody VehicleDTO vehicleDTO){
-		Vehicle newVehicle = vehicleService.createNewVehicle(vehicleDTO);	
+	public ResponseEntity<Vehicle> createNewUser(@Valid @RequestBody VehicleDTO vehicleDTO) {
+		Vehicle newVehicle = vehicleService.createNewVehicle(vehicleDTO);
 		return ResponseEntity.status(HttpStatus.CREATED).body(newVehicle);
-		
+
 	}
-	
+
 	@GetMapping
 	public ResponseEntity<Page<Vehicle>> getAllVehicles(@RequestParam(required = false) String typeVehicle,
-														@RequestParam(required = false) String stateVehicle,
-														@PageableDefault(size = 10, sort = "nameVehicle") Pageable pageable){
+			@RequestParam(required = false) String stateVehicle,
+			@PageableDefault(size = 10, sort = "nameVehicle") Pageable pageable) {
 		Page<Vehicle> vehicles = vehicleService.getAllVehicles(typeVehicle, stateVehicle, pageable);
 		return ResponseEntity.ok(vehicles);
+	}
+
+	// update de veiculos
+	@PutMapping("/{id}")
+	public ResponseEntity<Vehicle> updateVehicle(@PathVariable Long id, @Valid @RequestBody Vehicle vehicle) {
+
+		Vehicle updateVehicle = vehicleService.updateVehicle(id, vehicle);
+
+		return ResponseEntity.ok(updateVehicle);
+
+	}
+	
+	//deletar veiculo
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteVehicle(@PathVariable Long id){
+		 vehicleService.deleteVehicle(id);
+		return ResponseEntity.noContent().build();
 	}
 }

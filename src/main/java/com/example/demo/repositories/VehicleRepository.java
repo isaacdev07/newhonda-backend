6233b9ecb64b9f12bea17c9dfc73bbdddffd06ed
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-14 18:35:20 -0300

    Feat: add method to calculate the totalRevenue

--- src/main/java/com/example/demo/repositories/VehicleRepository.java
+++ src/main/java/com/example/demo/repositories/VehicleRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo.repositories;
 
+import java.math.BigDecimal;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.entities.Vehicle;
 import com.example.demo.enums.StateVehicle;
@@ -21,4 +24,9 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 	
 	//connta quantos veiculos tem com aquele tipo especifico
 	long countByTypeVehicle(TypeVehicle typeVehicle);
+	
+	//soma todos os preços de veiculos com o state sold
+	//o coalesce serve para retornar - ao inves de null caso nao tenha nenhum
+	@Query("SELECT COALESCE(SUM(v.price), 0) FROM Vehicle v WHERE v.stateVehicle = 'SOLD'")
+    BigDecimal getTotalRevenue();
 }
